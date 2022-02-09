e6056503a76c2fd17945b4ce32f81020c9ed7d2a
ghadi812004 <ghadi812004@gmail.com>
2022-02-09 14:20:47 +0200

    in main changed the background color ic_logindown changed the color and shape logoV is the new image

--- app/src/main/java/com/example/carbonjava/GamesFragment.java
+++ app/src/main/java/com/example/carbonjava/GamesFragment.java
@@ -1,5 +1,6 @@
 package com.example.carbonjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GamesFragment extends Fragment {
+    private androidx.cardview.widget.CardView firstGame;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_games, container, false);
+        View rootView =inflater.inflate(R.layout.fragment_games, container, false);
+        firstGame=rootView.findViewById(R.id.spaceShooter);
+        firstGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),TickTack.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 }
